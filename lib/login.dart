7682dda89d6743a3030589ff9cf7ab5dbb248f94
Demir Dereli <workdemirdereli@gmail.com>
2022-05-19 21:45:07 +0300

    Login, welcome page, sign up page

---
+++ lib/login.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class loginPage extends StatelessWidget {
+  const loginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: const Text(
+          'SPORTIFY',
+          style: TextStyle(
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Container(
+        color: Colors.black,
+        width: 500,
+        child: Column(
+          children: [
+            Container(
+              child: TextField(decoration: InputDecoration(hintText: "Email")),
+              color: Colors.white,
+              margin: const EdgeInsets.fromLTRB(20, 50, 20, 10),
+            ),
+            Container(
+              child:
+                  TextField(decoration: InputDecoration(hintText: "Password")),
+              color: Colors.white,
+              margin: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+            ),
+            Container(
+              height: 60,
+              width: 350,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.red, width: 5)),
+              margin: const EdgeInsets.fromLTRB(20, 30, 20, 10),
+              child: OutlinedButton(
+                onPressed: () {},
+                child: Text(
+                  "Log In",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25),
+                ),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
